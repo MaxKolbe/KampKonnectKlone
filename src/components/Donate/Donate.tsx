f264fb7c09b68828b3c55cc2d1b8bd57361dc3a7
Max <maximilianogbuabor@gmail.com>
2025-10-22 23:03:14 +0100

    commit 2

--- src/components/Donate/Donate.tsx
+++ src/components/Donate/Donate.tsx
@@ -1,26 +1,81 @@
 import "./Donate.css"
 import Navbar from "../Navbar/Navbar"
 import Footer from "../Footer/Footer"
-import Title from "../Title"
+import DON from "../../assets/DON.jpg"
+import { IoMdCheckmarkCircleOutline } from "react-icons/io"
+import { useState } from "react"
 
 
 const Donate = () => {
+interface ButtonData {
+    id: string
+    label: string
+}
+const cycleButtonItems: ButtonData[] = [
+  {id: 'btn-1', label: 'One-time'},
+  {id: 'btn-2', label: 'Monthly'},
+  {id: 'btn-3', label: 'Quaterly'},
+  {id: 'btn-4', label: 'Yearly'}
+]
+const sButtonItems: ButtonData[] = [
+  {id: 'btn-1', label: 'N250,000'},
+  {id: 'btn-2', label: 'N500,000'},
+  {id: 'btn-3', label: 'N750,000'},
+  {id: 'btn-4', label: 'N1,000,000'},
+  {id: 'btn-5', label: 'N2,000,000'},
+  {id: 'btn-6', label: 'N5,000,000'}
+]
+
+const [activeButtonId, setActiveButtonId] = useState<string | null>(cycleButtonItems[0].id)
+const [activeButtonIdS, setActiveButtonIdS] = useState<string | null>()
+
+const handleClick = (e: React.FormEvent, buttonId: string) => {
+    e.preventDefault()
+    setActiveButtonId(buttonId)
+} 
+const shandleClick = (e: React.FormEvent, buttonId: string) => {
+    e.preventDefault()
+    setActiveButtonIdS(buttonId)
+} 
+
     return(
         <>
         <Navbar />
-        <div >
-            <Title heading="Sponsor a Camper" subHeading="Your support can change a life. Help us nurture the next generation of female leaders." hideDisplay="none"/>
-            <div className="colorBar"></div>
-        </div>
-        <div>
-            <div>
-                <img src="" alt="" />
-                <h1></h1>
-                <p></p>
+        <div className="donateTitle">
+            <div className="donateBody">
+                <h1>Sponsor a Camper</h1>
+                <div className="colorBar"></div>
+                <p>Your support can change a life. Help us nurture the next generation of female leaders.</p>
             </div>
-            <div>
-                <form>
-
+        </div>
+        <div className="donateParent">
+            <div className="donateChild1">
+                <img src={DON} alt={DON} />
+                <h1>Your Support Can Change a Life</h1>
+                <p>By donating to Kamp Konnect, you're giving a young girl the opportunity to attend a week-long leadership and cultural experience designed to build her confidence, character, and skills. <br /><br />
+                Some of our applicants are bright, motivated students from under-resourced communities who would not be able to attend without financial support. Your donation goes directly toward covering their camp fees, materials, meals, and transportation.<br /><br />
+                Help us nurture the next generation of female leaders. Every contribution counts. Thank you.</p>
+            </div>
+            <div className="donateChild2">
+                <form className="donateForm">
+                    <h1><IoMdCheckmarkCircleOutline className="h1icon"/>Choose amount</h1>
+                    <div className="billCycle">
+                        {cycleButtonItems.map((button) => (
+                            <button key={button.id} className={activeButtonId === button.id ? 'cycle greenActive' : 'cycle'} onClick={(e) => handleClick(e, button.id)}>{button.label}</button>
+                        ))}
+                    </div>
+                     <div className="solidAmt">
+                        {sButtonItems.map((button) => (
+                            <button key={button.id} className={activeButtonIdS === button.id ? 'samt greenActive' : 'samt'} onClick={(e) => shandleClick(e, button.id)}>{button.label}</button>
+                        ))}
+                    </div>
+                    <div className="otheramt"><input type="text" placeholder="other amount" className="amt1" /> <span className="amt2">NGN</span></div>
+                    <input type="text" placeholder="Your full name*" />
+                    <input type="text" placeholder="Your email address*" />
+                    <input type="text" placeholder="Your phone number (optional)" />
+                    <label htmlFor="comment"><IoMdCheckmarkCircleOutline className="labelicon"/> Add note/comment</label>
+                    <textarea name="comment" id="" placeholder="Enter your message for kamp konnect..." rows={3 }></textarea>
+                    <button className="paybtn">Confirm with N0</button>
                 </form>
             </div>
         </div>
--- src/components/Participate/Participate.tsx
+++ src/components/Participate/Participate.tsx
@@ -1,13 +1,51 @@
 import "./Participate.css"
 import Navbar from "../Navbar/Navbar"
 import Footer from "../Footer/Footer"
+import { BsFillPersonXFill } from "react-icons/bs"
+import { FaRegHeart } from "react-icons/fa6";
+import { MdOutlineEmail } from "react-icons/md";
 
 const Participate = () => {
     return(
         <>
-         <Navbar />
-            <div>Participate</div>
-             <Footer />
+        <Navbar />
+            <div className="participateTitle">
+                <div className="participateBody">
+                    <h1>Kamp Konnect 2025</h1>
+                    <div className="colorBar"></div>
+                </div>
+                {/* IS REGISTRATION OPEN? */}
+                <div className="regStatus">
+                    <BsFillPersonXFill className="regStatusIcon" />
+                    <h4>Registration is Currently Closed</h4>
+                </div>
+
+                {/* NEXT YEAR? */}
+                <form action="" className="nyForm">
+                    <FaRegHeart className="nyIcon"/>
+                    <h4>Interested in Next Year?</h4>
+                    <p>Be the first to know when registration opens for Kamp Konnect 2026! Submit your information below and we'll notify you as soon as registration becomes available.</p>
+                    <label htmlFor="">Parent's Primary Contact Email Address *</label>
+                    <input type="text" name="" id="" placeholder="parent@example.com"/>
+                    <label htmlFor="">Parent/Guardian Full Name *</label>
+                    <input type="text" name="" id="" placeholder="Parent/Guardian Full Name" />
+                    <label htmlFor="">Contact Number (Parent/Guardian) *</label>
+                    <input type="text" name="" id="" placeholder="e.g. +234 XXX XXX XXXX" />
+                    <span className="nyFormSpan" style={{fontSize: ".8rem"}}>Include country code</span>
+                    <button className="btn-reverse" style={{width: "100%"}}> <MdOutlineEmail/> Submit Interest</button>
+                </form>
+
+                {/* STAY CONNECTED */}
+                <div className="stayConnected">
+                    <h6>Stay Connected</h6>
+                    <p>Follow us on social media for camp updates, highlights from previous years, and announcements about next year's registration.</p>
+                    <div className="stayConBtn">
+                        <a href="" className="stayConBtn">Follow on Instagram</a>
+                        <a href="" className="stayConBtn">Like on Facebook</a>
+                    </div>
+                </div>
+            </div>
+        <Footer />
         </>
     )
 }
